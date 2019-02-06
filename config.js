cba5c111eb95959f7b6e6153cd3cced79e36c191
DamianM <damian.michalski@kramp.com>
2019-02-06 12:26:54 +0100

    ExpressJS 25.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '74685644594-clhrs7u06c0dnk74pbt74pm2nmqtigaj.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'xBLjaqETk3GLWVEahRCfce1P',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback' 
+};
